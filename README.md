059e8d8932c4a4713b94199e231fe12d97dd9c7e
jmcgarr <jmcgarr@fexco.com>
2022-03-08 16:37:28 +0000

    Added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Website README
+
+ - <a href="https://jamesdaniel.github.io/">https://jamesdaniel.github.io/</a>
---
+++ source.sh
@@ -0,0 +1,2 @@
+git config --global user.name "JamesDaniel"
+git config --global user.email "jamesmcgarr@protonmail.com"
