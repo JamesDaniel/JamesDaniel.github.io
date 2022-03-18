3ab0ef70f7b467755f40a59d33a9a1006f1c1bc1
JamesDaniel <jamesmcgarr@protonmail.com>
2022-03-18 23:28:17 +0000

    Fixed nav bar which broke when upgraded from bootstrap 4 to 5

--- nav_bar.js
+++ nav_bar.js
@@ -4,23 +4,24 @@ const e = React.createElement;
 
 class NavBar extends React.Component {
   render() {
-    return <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Portfolio Website</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="java_patterns.html">Design Patterns in Java</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+    return  <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+              <div class="container-fluid">
+                <a class="navbar-brand" href="javascript:void(0)">Portfolio Website</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="mynavbar">
+                  <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                      <a class="nav-link" href="index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="java_patterns.html">Design Patterns in Java</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
   }
 }
 
