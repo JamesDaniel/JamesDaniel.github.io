9f8421a61eaf9c9ee740ba20b1ce6b92e1884843
JamesDaniel <james@jamesmcgarr.com>
2025-02-09 15:16:36 +0000

    Fixed up menu styling a little

---
+++ menu_v2.js
@@ -0,0 +1,6 @@
+
+$( document ).ready(function() {
+  $('.menu_btn_container')    .height( $('.menu_btn_container').width()     );
+  $('.menu_btn_img_container').height( $('.menu_btn_container').height()*.9 );
+  $('.menu_btn_footer')       .height( $('.menu_btn_container').height()*.1 );
+});
