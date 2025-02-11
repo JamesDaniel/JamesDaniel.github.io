db2cd41896418d842d2dd8a30a6f214fe43921fe
JamesDaniel <james@jamesmcgarr.com>
2025-02-11 23:18:46 +0000

    Added a resize listener for the presentation iframe in bathroom vent project

--- bathroom_vent_project.js
+++ bathroom_vent_project.js
@@ -2,4 +2,9 @@
 $( document ).ready(function() {
   $('#iframePresentationId').width(document.body.clientWidth);
   $('#iframePresentationId').height(window.innerHeight-8);
+
+  window.addEventListener("resize", () => {
+    $('#iframePresentationId').width( document.body.clientWidth);
+    $('#iframePresentationId').height(window.innerHeight-8);
+  });
 });
