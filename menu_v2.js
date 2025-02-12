db8cd91f6d76655f8718542ecf4a91bd1ee14340
JamesDaniel <james@jamesmcgarr.com>
2025-02-12 20:02:56 +0000

    Fixed image overflow on small screens

--- menu_v2.js
+++ menu_v2.js
@@ -3,4 +3,5 @@ $( document ).ready(function() {
   $('.menu_btn_container')    .height( $('.menu_btn_container').width()     );
   $('.menu_btn_img_container').height( $('.menu_btn_container').height()*.9 );
   $('.menu_btn_footer')       .height( $('.menu_btn_container').height()*.1 );
+  $('.menu_btn_img_container').css('padding',($('.menu_btn_img_container').width()*0.1)+'px');
 });
